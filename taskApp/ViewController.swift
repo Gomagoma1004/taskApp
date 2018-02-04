ae9dbebb3d7d848162fa9e80d16fddd9367a435e
Gomagoma1004 <32089543+Gomagoma1004@users.noreply.github.com>
2018-02-05 00:03:41 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 勝良祥吾 on 2018/02/05.
+//  Copyright © 2018年 shougo.katsura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
